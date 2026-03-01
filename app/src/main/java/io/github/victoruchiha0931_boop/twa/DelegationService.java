b9e0b109190432da93ee4aa8ada9edfe5c898b03
Victor <victoruchiha0931@gmail.com>
2026-03-01 20:50:22 -0300

    apk teste

---
+++ app/src/main/java/io/github/victoruchiha0931_boop/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.victoruchiha0931_boop.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
